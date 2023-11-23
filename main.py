3195fcd8a261dc9c81bc3ebc192c31b14aea2ae4
Jorge Vitor <103287884+JorgeVitor30@users.noreply.github.com>
2023-11-23 17:37:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from PyPDF2 import PdfReader
 
 class Ocr:
@@ -6,8 +7,9 @@ class Ocr:
         self.pdf = PdfReader(dir_pdf)
 
     def process(self):
-        # Onde todos os processos são interligados
-        pass
+        dados = self.extraction()
+        self.transform_to_df(dados)
+
 
     def returning_pages(self):
         # Quantidade total de páginas no PDF
@@ -19,24 +21,31 @@ class Ocr:
         return qtd_page
 
     def extraction(self):
-
-        for i in range(2):
+        all_list = []
+        for i in range(self.returning_pages()):
+            temp_aux = []
             page = self.pdf.pages[i].extract_text()
 
             validate_page = page.find('BANCO DO NORDESTE')
 
             if validate_page != -1:
-                list_data = []
 
                 index_processo = page.find('Processo')
                 index_data_disp = page.find('Data Disponibilização::')
                 index_data_publi = page.find('Data Publicação::')
+                index_data_publi2 = page.find('Data Publ')
                 index_jornal = page.find('Jornal:')
                 index_tribunal = page.find('Tribunal:')
                 index_vara = page.find('Vara:')
                 index_cidade =page.find('Cidade:')
                 index_pagina = page.find('Página:')
                 index_titulo = page.find('Título:')
+
+                number_aux_publi = 17
+
+                if index_data_publi == -1:
+                    index_data_publi = index_data_publi2
+                    number_aux_publi += 1
 
 
                 id_processo = page[(index_processo+9):index_data_disp].replace(' ', '').strip()
@@ -45,38 +54,40 @@ class Ocr:
 
                 id_compromisso = id_processo + ' ' + data_dispo
 
-                print(id_compromisso)
 
-                data_publi = page[index_data_publi+17:index_jornal].strip()
-                print(data_publi)
-
+                data_publi = page[index_data_publi+number_aux_publi:index_jornal].strip()
                 jornal = page[index_jornal+7:index_tribunal].strip()
-                print(jornal)
-
                 tribunal = page[index_tribunal+9:index_vara].strip()
-                print(tribunal)
-
                 vara = page[index_vara + 5:index_cidade].strip()
-                print(vara)
-
                 cidade = page[index_cidade+7:index_pagina].strip()
-                print(cidade)
-
                 pagina = page[index_pagina+7:index_titulo].strip()
-                print(pagina)
-
                 titulo = page[index_titulo:]
-                print(titulo)
+
+                temp_aux.append(id_compromisso)
+                temp_aux.append(data_publi)
+                temp_aux.append(jornal)
+                temp_aux.append(tribunal)
+                temp_aux.append(vara)
+                temp_aux.append(cidade)
+                temp_aux.append(pagina)
+                temp_aux.append(titulo)
+
+                all_list.append(temp_aux)
+
+        return all_list
+
+    def transform_to_df(self, lista):
+        df = pd.DataFrame()
 
 
-                print('\n')
-
-
-
-
+        colunas = ["id_compromisso", "data_publicacao", "jornal", "tribunal", "vara", "cidade", "pagina", "titulo"]
+        df = pd.DataFrame(lista, columns=colunas)
+        print(df)
+        df.to_excel('teste.xlsx')
 
 process = Ocr('publicacoes_42_90554530_221215_TJCE_20221215_085343.pdf')
-process.returning_pages()
-process.extraction()
+process.process()
+
+
 
 #TODO FAZER UMA CLASSE DE VERIFICAÇÃO ARQUIVO WORD (CONVERSÃO PARA PDF)
